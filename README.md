4925012966de4812ce0953b308f6544573d293da
Nefer <nefer.suarez@udc.es>
2025-07-18 16:54:04 +0200

    SMA

--- README.md
+++ README.md
@@ -3,6 +3,14 @@ This project aims to forecast stock/crypto price trends using CSV data, visualiz
 
 `This project is packaged, so you can use it locally in your computer.`
 
+## Install dependencies
+
+#### Using (venv)
+`python3 -m pip install -r requirements.txt`
+
+#### Using pip
+`pip install -r requirements.txt`
+
 ## Tech Stack
 
 | Component |  Tools  |
--- eda.py
+++ eda.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
+import seaborn as sns
 
 def loadAndClean(symbol):
     path = f"data/{symbol}.csv"
@@ -22,7 +23,15 @@ def plotClose(df, symbol):
     row = 10
     col = 4
     plt.figure(figsize=(row,col))
-    plt.plot(df["Close"], label=f"{symbol} Close Price")
+
+    #Simple Moving Average
+    df["SMA50"] = df["Close"].rolling(window=50).mean()
+    df["SMA200"] = df["Close"].rolling(window=200).mean()
+
+    plt.plot(df["Close"], label="Close Price", color="blue")
+    plt.plot(df["SMA50"], label="SMA 50", color="orange")   
+    plt.plot(df["SMA200"], label="SMA 200", color="green")  
+
     plt.title(f"{symbol} - Close Price")
     plt.xlabel("Date")
     plt.ylabel("Price ($)")
@@ -31,8 +40,15 @@ def plotClose(df, symbol):
     plt.tight_layout()
     plt.show()
 
+def showNullData(df,symbol):
+    sns.heatmap(df.isnull(), cbar=False)
+    plt.title(f"{symbol} - Missing Data Map")
+    plt.show()
+
 if __name__ == "__main__":
     symbols = ["AAPL", "TSLA", "BTC-USD", "ETH-USD"]
     for symbol in symbols:
         df = loadAndClean(symbol)
+        #showNullData(df, symbol)
         plotClose(df, symbol)
+        print(df.describe())
